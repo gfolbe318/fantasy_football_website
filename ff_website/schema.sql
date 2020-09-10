8fc2799ec248f35cdfe0d9835943a0c2cf942728
gfolbe318 <gmfolbe@yahoo.com>
2020-09-10 02:21:00 -0400

    reformatted table

--- ff_website/schema.sql
+++ ff_website/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS members;
 
 CREATE TABLE members
 (
-    id INTEGER PRIMARY KEY ID AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
     year_joined INTEGER,
