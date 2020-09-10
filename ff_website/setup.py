4fa152362bb455fb812e39c8511353e22e5061f3
gfolbe318 <gmfolbe@yahoo.com>
2020-09-10 02:21:22 -0400

    packages commented for now

---
+++ ff_website/setup.py
@@ -0,0 +1,21 @@
+# from setuptools import find_packages, setup
+
+# setup(
+#     name="ff_website.py",
+#     version="1.0.0",
+#     packages=find_packages(),
+#     install_requires=[
+#         "autopep8",
+#         "click",
+#         "Flask",
+#         "Flask-CLI",
+#         "itsdangerous",
+#         "Jinja2",
+#         "MarkupSafe",
+#         "pip",
+#         "pycodestyle",
+#         "setuptools",
+#         "toml",
+#         "Werkzeug"
+#     ],
+# )
