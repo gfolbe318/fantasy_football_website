939dca2710060ae00eeb577efe8f4ece7a01c91d
gfolbe318 <gmfolbe@yahoo.com>
2020-09-12 17:43:24 -0400

    deleted setup.py old version

--- ff_website/index.py
+++ ff_website/index.py
@@ -5,21 +5,9 @@ from ff_website import app
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    conn = get_db()
-    conn.execute("""
-        INSERT INTO members 
-        (firstname, lastname, year_joined, active)
-        VALUES (?, ?, ?, ?)""",
-                 ("Garrett", "Folbe", 2016, 1)
-                 )
-    conn.commit()
     return "Hello world!"
 
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
-    conn = get_db()
-    conn.execute("DELETE FROM members WHERE firstname = ?", ("Garrett",))
-    conn.commit()
-    conn.close()
     return "Hello world"
