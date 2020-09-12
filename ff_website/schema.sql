91a20ef5e74ea552b68fc779308dd0002b8677a7
gfolbe318 <gmfolbe@yahoo.com>
2020-09-12 17:40:11 -0400

    added game table

--- ff_website/schema.sql
+++ ff_website/schema.sql
@@ -1,10 +1,30 @@
-DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS game;
 
-CREATE TABLE members
+CREATE TABLE member
 (
-    id INTEGER PRIMARY KEY,
+    member_id INTEGER PRIMARY KEY,
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
-    year_joined INTEGER,
-    active INTEGER
+    year_joined INTEGER NOT NULL,
+    active INTEGER NOT NULL
 );
+
+
+CREATE TABLE game
+(
+    game_id INTEGER PRIMARY KEY,
+    team_A_score FLOAT NOT NULL,
+    team_B_score FLOAT NOT NULL,
+    matchup_date TEXT NOT NULL,
+    week INTEGER NOT NULL,
+    matchup_length INTEGER NOT NULL,
+    playoffs INTEGER NOT NULL,
+    consolation INTEGER NOT NULL,
+    team_A_id INTEGER NOT NULL,
+    team_B_id INTEGER NOT NULL,
+    FOREIGN KEY (team_A_id) REFERENCES member (member_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (team_B_id) REFERENCES member (member_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- https://stackoverflow.com/questions/25675314/how-to-backup-sqlite-database
