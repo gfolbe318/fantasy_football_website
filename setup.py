d1c8133e37fec08a15dca0edc1fa33dc7b4fe180
gfolbe318 <gmfolbe@yahoo.com>
2020-09-12 17:34:58 -0400

    removed table from git

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ff_website",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "autopep8",
+        "click",
+        "flask",
+        "flask-CLI",
+        "itsdangerous",
+        "jinja2",
+        "MarkupSafe",
+        "pip",
+        "pycodestyle",
+        "setuptools",
+        "toml",
+        "Werkzeug"
+    ],
+)
