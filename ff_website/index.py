cb611548f6cf989277f432cab3d9f59849b508b6
gfolbe318 <gmfolbe@yahoo.com>
2020-09-20 19:26:02 -0400

    added decsription issues

--- ff_website/index.py
+++ ff_website/index.py
@@ -5,14 +5,37 @@ from ff_website import app
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    return render_template("home.html")
+    links = [
+        {
+            "title": "Current Season",
+            "img_file": url_for("static", filename="img/current_season.png"),
+            "description": "Find more information on this current season, including: standings, statistics, and more."
+        },
+        {
+            "title": "Archives",
+            "img_file": url_for("static", filename="img/archives.png"),
+            "description": "Browse through members of previous seasons, pull up historical results, and compare league members head to head."
+        },
+        {
+            "title": "Hall of Fame",
+            "img_file": url_for("static", filename="img/hall_of_fame.png"),
+            "description": "View the names of the enshrined members of our league who have etched their names in the history books."
+        },
+        {
+            "title": "League Office",
+            "img_file": url_for("static", filename="img/espn.png"),
+            "description": "Visit our official home page on ESPN for your complete fantasy football experience."
+        }
+    ]
+
+    return render_template("home.html", ql=links)
 
 
 @app.route("/members", methods=["GET", "POST"])
 def members():
     photo = "static/img/gmf.jpg"
     l = [photo for _ in range(12)]
-    return render_template("members.html",  photos=l)
+    return render_template("league_members.html",  photos=l)
 
 
 @app.route("/test", methods=["GET", "POST"])
